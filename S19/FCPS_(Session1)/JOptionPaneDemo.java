8f48de738620668230d149b29aa215deeaaabd4c
dkimpara <dhammakimpara@gmail.com>
2021-07-15 11:47:40 -0600

    first try

--- S19/FCPS_(Session1)/JOptionPaneDemo.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class JOptionPaneDemo{
-   
-    public static void main(String[] args){
-		String input = JOptionPane.showInputDialog(null, "What is your name?");
-		JOptionPane.showMessageDialog(null, "Hello, " + input);
-    }
-}
--- S19/FCPS_(Session1)/ParseDemo.java
+++
@@ -1,8 +0,0 @@
-public class ParseDemo{
-
-	public static void main(String[] args){		
-		String x = "123.5";
-		int y = Integer.parseInt(x);
-		
-	}
-}
